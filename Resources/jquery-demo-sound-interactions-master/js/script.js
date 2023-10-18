1b7122ca38353276979223c28a31d19a3784b3b4
TaborCote <146276002+TaborCote@users.noreply.github.com>
2023-10-18 14:25:21 -0700

    Fixed some fonts, created more tags and fixed the main logo

---
+++ Resources/jquery-demo-sound-interactions-master/js/script.js
@@ -0,0 +1,18 @@
+
+$('.clickme').click(function(){
+		$('audio#owl')[0].play()
+
+})
+
+$('.pauseme').click(function(){
+	$('audio#owl')[0].pause()
+})
+
+$('.restartme').click(function(){
+	$('audio#owl')[0].currentTime = 0
+	$('audio#owl')[0].play()
+})
+
+$('.text-btn').click(function(){
+	$('.text').toggleClass('hide')
+})
